0b6ecc4bab40289bff5a463b7c8464c3d73b42ae
zhou109 <zhou109@Illinois.edu>
2018-09-19 16:47:35 -0500

    Intial project commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
 public class Main {
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
+    }
 
     public static void main(String[] args) {
+        System.out.println(urlToString("https://www.bls.gov/tus/charts/chart9.txt"));
+        System.out.println('c');
 	// write your code here
     }
 }
