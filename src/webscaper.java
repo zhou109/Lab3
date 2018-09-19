57811d8982f82c15abcc764bd31cd35a4b818356
zhou109 <zhou109@Illinois.edu>
2018-09-19 16:22:19 -0500

    Intial project commit

--- src/webscaper.java
+++ src/webscaper.java
@@ -1,4 +1,25 @@
-public class webscaper {
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+public class webscaper{
+
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
+    }
     public static void main(String[] unused) {
         System.out.println("Ran");
     }
